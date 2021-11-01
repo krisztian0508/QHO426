c92a4ea57b02300a652bdc9d333273acef5c4fb0
czgone <>
2021-11-01 15:20:28 +0000

    Added code to display escape characters.

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-print("System Failure Imminent!")
-print("System reboot has been initiated...")
-print()
-print()
-print("...rebooting sensory system")
-print("...rebooting output motors")
-print("...rebooting hover engine")
-print()
-print()
-print("System online.")
+
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
