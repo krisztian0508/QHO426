2697079a80cf370b637a9f1f01f1ca8632b6594a
czgone <93393653+krisztian0508@users.noreply.github.com>
2021-11-06 18:47:35 +0000

    reverse update

--- basic/repetition/for_loop/reverse.py
+++ basic/repetition/for_loop/reverse.py
@@ -2,7 +2,7 @@
 phrase = input("What phrase do you see?\n")
 
 # Identify markings
-print("\nReversing...\nThe phrase is", end="")
+print("\nReversing...\nThe phrase is: ", end="")
 
 for position in range(len(phrase) - 1, -1, -1):
     print(phrase[position], end="")
