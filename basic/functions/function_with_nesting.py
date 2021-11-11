3d34465869cab0c2858596572b05b7fcd92424c8
czgone <93393653+krisztian0508@users.noreply.github.com>
2021-11-11 19:02:39 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basic/functions/function_with_nesting.py
@@ -0,0 +1,10 @@
+
+def identify():
+  sound= input("\nWhat do you see?\n")
+  if sound == "a large boulder":
+       print("It's time to run!")
+  else:
+   print("We will be fine.")  
+
+# Calls to the function
+identify()
