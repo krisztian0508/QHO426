901dd7ae65e9f4813d14edaaab7ab727e87ce24c
czgone <93393653+krisztian0508@users.noreply.github.com>
2021-11-16 19:59:44 +0000

    Added code to demonstrate the use of multiple function calls.

---
+++ data/sets/simple_set.py
@@ -0,0 +1,12 @@
+#The first function should be named observed and should have no parameters. 
+def observed():
+  #I created a set named observations. 
+  observations = {"Flying Car", "Sky Scraper", "Laser", "Dome"}
+  #function should return the set observations. 
+  return observations
+#second function
+def run():
+  #will print first function
+  print(observed())
+#call the function run will actually will print the first function's set
+run()
